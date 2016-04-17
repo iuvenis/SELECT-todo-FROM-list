8b7684862c889be263872364f94d421e7947a057
tpyoung <tylerparkeyoung@gmail.com>
2016-04-16 19:14:39 -1000

    Tables created

--- todo_app.sql
+++ todo_app.sql
@@ -15,8 +15,23 @@ CREATE DATABASE todo_app;
 
 -- query to create a table named tasks using the Initial columns detailed below
 CREATE TABLE task (
-id SERIAL NOT NULL PRIMARY KEY
+id INTEGER NOT NULL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+description TEXT NULL,
+created_at TIMESTAMP NOT NULL DEFAULT now(),
+updated_at TIMESTAMP NULL,
+completed BOOL NOT NULL DEFAULT FALSE
 );
+
 ALTER TABLE task DROP COLUMN IF EXISTS completed;
 ALTER TABLE task ADD COLUMN completed_at TIMESTAMP WITH TIME ZONE DEFAULT NULL;
-ALTER TABLE task UPDATE COLUMN updated_at NOT NULL DEFAULT now();
+ALTER TABLE task ALTER COLUMN updated_at SET DEFAULT now();
+ALTER TABLE task ALTER COLUMN updated_at SET NOT NULL;
+
+UPDATE task
+SET id = default,
+    title = 'Study SQL',
+    description = 'complete this exercise',
+    created_at = now(),
+    updated_at = now(),
+    completed_at = now();
